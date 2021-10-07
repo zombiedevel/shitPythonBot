546d9d51e8818a8c80cf65079c89b0ed39e53494
valery <valery.a@jsexpert.ru>
2021-10-07 19:07:55 +0300

    upd

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ API_SERVER = TelegramAPIServer.from_base(os.getenv("API_SERVER", "https://api.te
 
 # Database configuration
 
-DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
-DB_USER = os.getenv("DB_USER", "root")
-DB_PASS = os.getenv("DB_PASS", "admin2501")
-DB_NAME = os.getenv("DB_NAME", "crypto")
+DB_HOST = 'mysql'
+DB_USER = 'docker'
+DB_PASS = 'docker'
+DB_NAME = 'cryptodb'
 
 
